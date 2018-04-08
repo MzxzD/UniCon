386d0c2eeba8bb98e222d9bcf0ede7c7421b9240
Mateo Došlić <MzxzD@Users-MacBook-Pro.local>
2018-04-08 21:34:22 +0200

    Added UI buttons, new ViewControler for results.

---
+++ UniCon/MassViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MassViewController.swift
+//  UniCon
+//
+//  Created by Mateo Došlić on 08/04/2018.
+//  Copyright © 2018 Mateo Došlić. All rights reserved.
+//
+
+import UIKit
+
+class MassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
